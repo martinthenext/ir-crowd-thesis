4b6194980fd3cfc6c4f3fbc3e649e3b03cc140cc
Martin Davtyan <martin.davtyan@gmail.com>
2014-11-11 15:10:55 +0100

    First version of accuracy sequence

--- experiments.py
+++ experiments.py
@@ -6,15 +6,20 @@ import random
 
 TOPIC_ID = '20690'
 
-def get_accuracy(estimate_list, list_with_Nones):
+def get_accuracy(estimates, truths):
   """ 
   This gets boolean lists of estimates and truths with corresponding
   positions and returns a fraction of matching items
+
+  If any of the pair (estimate, truth) is None, it is disregarded
   """
-  pairs = izip(estimate_list, list_with_Nones)
-  pairs_without_Nones = ifilter(lambda x: x[1] is not None, pairs)
+  pairs = izip(estimates, truths)
+  pairs_without_Nones = ifilter(lambda x: None not in x, pairs)
   matching = [x == y for (x, y) in pairs_without_Nones]
-  return np.mean(matching)
+  if not matching:
+    return None
+  else:
+    return np.mean(matching)
 
 def get_majority_vote(vote_list):
   """ 
@@ -44,20 +49,28 @@ estimates = [get_majority_vote(vote_list) for vote_list in vote_lists]
 print get_accuracy(estimates, truths)
 
 def get_accuracy_sequence(n_votes_to_sample, vote_lists, truths):
-  """ Randomly sample votes 
+  """ Randomly sample votes and re-calculate estimates
   """
-  unknown_votes = deepcopy(vote_lists)
-  known_votes = []
+  unknown_votes = copy_and_shuffle_sublists(vote_lists)
+  known_votes = [ [] for _ in unknown_votes ]
 
-  accuracy_sequence = np.zeros(n_votes_to_sample)
+  estimates = [None for _ in vote_lists]
+
+  accuracy_sequence = [None] * n_votes_to_sample
+
   for index in xrange(n_votes_to_sample):
-    # draw one vote for a random document
-    pass
+    # Draw one vote for a random document
+    updated_doc_idx = random.randrange(len(vote_lists))
+    if not unknown_votes[updated_doc_idx]:
+      # We ran out of votes for this document, stop sequencing
+      continue
+    vote = unknown_votes[updated_doc_idx].pop()
+    known_votes[updated_doc_idx].append(vote)
+    # Recalculate the estimate on the affected document
+    estimates[updated_doc_idx] = get_majority_vote(known_votes[updated_doc_idx])
+    # Calucate the accuracy
+    accuracy_sequence[index] = get_accuracy(estimates, truths)
 
-"""
-for iteration in max_iterations:
-  sample vote
-  update estimates
-  compare estimates with truths
+  return accuracy_sequence
 
-"""
+print get_accuracy_sequence(100, vote_lists, truths)

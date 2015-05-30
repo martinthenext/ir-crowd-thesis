66ec29b8d815d04829939f5dbff9775a28a0d09d
Martin Davtyan <martin.davtyan@gmail.com>
2015-05-30 20:31:19 +0200

    rungp.m takes serialized matrices and runs GPs on them

---
+++ matlab/genTestData.m
@@ -0,0 +1,18 @@
+% Generate data
+n1 = 80; n2 = 40;
+S1 = eye(2); S2 = [1 0.95; 0.95 1];
+m1 = [0.75; 0]; m2 = [-0.75; 0];
+x1 = bsxfun(@plus, chol(S1)'*gpml_randn(0.2, 2, n1), m1);
+x2 = bsxfun(@plus, chol(S2)'*gpml_randn(0.3, 2, n2), m2);
+
+x = [x1 x2]'; 
+y = [-ones(1,n1) ones(1,n2)]';
+
+% Test data
+[t1 t2] = meshgrid(-4:0.1:4,-4:0.1:4);
+t = [t1(:) t2(:)]; 
+n = length(t);
+
+% Saving
+save('train.mat', 'x', 'y');
+save('test.mat', 't');
--- matlab/rungp.m
+++ matlab/rungp.m
@@ -1,20 +1,8 @@
 % Include subdirectories to use GPML code
 addpath(genpath('./'))
 
-% Generate data
-n1 = 80; n2 = 40;
-S1 = eye(2); S2 = [1 0.95; 0.95 1];
-m1 = [0.75; 0]; m2 = [-0.75; 0];
-x1 = bsxfun(@plus, chol(S1)'*gpml_randn(0.2, 2, n1), m1);
-x2 = bsxfun(@plus, chol(S2)'*gpml_randn(0.3, 2, n2), m2);
-
-x = [x1 x2]'; 
-y = [-ones(1,n1) ones(1,n2)]';
-
-% Test data
-[t1 t2] = meshgrid(-4:0.1:4,-4:0.1:4);
-t = [t1(:) t2(:)]; 
-n = length(t);
+load('train.mat');
+load('test.mat');
 
 % Train
 meanfunc = @meanConst; 
@@ -24,9 +12,9 @@ hyp.cov = [];
 likfunc = @likErf;
 
 hyp = minimize(hyp, @gp, -40, @infEP, meanfunc, covfunc, likfunc, x, y);
-[a b c d lp] = gp(hyp, @infEP, meanfunc, covfunc, likfunc, x, y, x, ones(120, 1));
+[n, nn] = size(t)
+[a b c d lp] = gp(hyp, @infEP, meanfunc, covfunc, likfunc, x, y, t, ones(n, 1));
 % This gives a probability of y = +1
 prob = exp(lp);
 
-
-
+disp( prob(1, :) )

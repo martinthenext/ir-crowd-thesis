27ddcfc1dbfb3ffa43b105ee8a2af786873be584
Martin Davtyan <martin.davtyan@gmail.com>
2015-06-30 01:08:23 +0200

    Calculating accuracy means

--- run_on_euler.sh
+++ run_on_euler.sh
@@ -5,7 +5,7 @@
 # This file contains a command that has been run after the 
 # corresponding code has been commited to the repo
 
-N_RUNS_PER_TOPIC=1
+N_RUNS_PER_TOPIC=30
 
 topic_ids=(20932 20488 20910 20958 20714 20636 20956 20424 20916 20542 20778 20690 20696 20694 20832 20962 20812 20814 20704 20922 20780 20766 20644 20764 20642 20686 20976 20972 20584 20996)
 
@@ -16,7 +16,7 @@ do
   do
   
     runid=$RANDOM
-    bsub -o run_output/out_run_$runid -e accuracy_1_vote_per_doc -R "rusage[mem=2000]" "python -W ignore oneaccuracy.py $topic_id" 
+    bsub -o /dev/null -e accuracy_1_vote_per_doc -R "rusage[mem=2000]" "python -W ignore oneaccuracy.py $topic_id" 
 
   done
 done

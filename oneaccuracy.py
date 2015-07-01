57a7e736087aa22b2968e385dda336707a013523
Martin Davtyan <martin.davtyan@gmail.com>
2015-07-01 15:33:02 +0200

    Run unidied experiment for different methods

--- oneaccuracy.py
+++ oneaccuracy.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     raise Error("Please suppy topic_id to get accuracies for")
   
   print_accuracies_to_stderr({
-#    'Matlab GP' : (est_gp, [None])
+     'Matlab GP' : (est_gp, [None]),
      'MajorityVote' : (est_majority_vote, []),
      'MergeEnoughVotes(1)' : (est_merge_enough_votes, [ 1 ]),
      'MajorityVoteWithNN(0.5)' : (est_majority_vote_with_nn, [ 0.5 ]),
--- run_on_euler.sh
+++ run_on_euler.sh
@@ -5,7 +5,7 @@
 # This file contains a command that has been run after the 
 # corresponding code has been commited to the repo
 
-N_RUNS_PER_TOPIC=30
+N_RUNS_PER_TOPIC=20
 
 # Excluding the loser topics: 20644 and 20922
 topic_ids=(20932 20488 20910 20958 20714 20636 20956 20424 20916 20542 20778 20690 20696 20694 20832 20962 20812 20814 20704 20780 20766 20764 20642 20686 20976 20972 20584 20996)
@@ -17,7 +17,7 @@ do
   do
   
     runid=$RANDOM
-    bsub -o /dev/null -e accuracy/exp-gp-accuracy-1-vote-per-doc.tsv -R "rusage[mem=2000]" "python -W ignore oneaccuracy.py $topic_id" 
+    bsub -o /dev/null -e accuracy/exp-accuracy-1-vote-per-doc.tsv -R "rusage[mem=2000]" "python -W ignore oneaccuracy.py $topic_id" 
 
   done
 done

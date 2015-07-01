504e3b30ed71a52ef97618259895b25f5c9c9bd3
Martin Davtyan <martin.davtyan@gmail.com>
2015-07-01 20:40:29 +0200

    GP data filtered, for example purposes, to fix errors

--- accuracy/filter-and-append.sh
+++ accuracy/filter-and-append.sh
@@ -4,5 +4,8 @@
 # ARGUMENT 1: input file, from the stderr of Euler jobs
 # ARGUMENT 2: output file, to which clean data gets appended
 
+# Example 
+# ./filter-and-append.sh exp-gp-accuracy-1-vote-per-doc.tsv exp-gp-accuracy-1-vote-per-doc.tsv.new 
+
 grep -P '^.*\t.*\t.*$' $1 >> $2
 

f6a39769d2b41f2d2d2a4079c0c67298f358c154
Martin Davtyan <martin.davtyan@gmail.com>
2015-07-26 15:58:45 +0200

    Let us omit output anyway

--- run_accuracy_seq_on_euler.sh
+++ run_accuracy_seq_on_euler.sh
@@ -17,13 +17,16 @@ batch_id=$RANDOM
 topic_ids=(20932 20488 20910 20958 20714 20636 20956 20424 20916 20542 20778 20690 20696 20694 20832 20962 20812 20814 20704 20780 20766 20764 20642 20686 20976 20972 20584 20996)
 #topic_ids=(20910)
 
+#OUTPUT_FILE="run_output/stdout-${batch_id}"
+OUTPUT_FILE="/dev/null"
+
 for topic_id in "${topic_ids[@]}"
 do
 
   for job in `seq 1 $N_JOBS_PER_TOPIC`;
   do
 
-  bsub -W 120:00 -o "run_output/stdout-${batch_id}" -e "accuracy/${OUTPUT_NAME}.stderr-${batch_id}.tsv" -R "rusage[mem=16000]" "python -W ignore accuracy_seq.py $topic_id" 
+  bsub -W 120:00 -o $OUTPUT_FILE -e "accuracy/${OUTPUT_NAME}.stderr-${batch_id}.tsv" -R "rusage[mem=16000]" "python -W ignore accuracy_seq.py $topic_id" 
 
   done
 

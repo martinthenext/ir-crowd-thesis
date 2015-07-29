8af2660c2f35f8fc58d1901a0bb732ef43acfdb3
Martin Davtyan <martin.davtyan@gmail.com>
2015-07-29 15:29:13 +0200

    More experimental data on accuracies merged for significance tests

--- accuracy/after-random-votes/pick-first-accuracies.py
+++ accuracy/after-random-votes/pick-first-accuracies.py
@@ -4,16 +4,29 @@ This script takes first iterations from the stardard accuracy file
 
 """
 import sys
+import re
+
+column_regexps = [
+  'AC',
+  '\d+',
+  '\d+',
+  '[^\t]+',
+  '\d+',
+  '[0-9\.]+',
+]
+
+regex = '\t'.join( ['(%s)' % s for s in column_regexps] )
 
 with open(sys.argv[1], 'r') as f:
   current_method = ""
   for line in f:
-    try:
-      ac, iter, run_id, method, topic, accuracy = line.split('\t')
-      
-      if method != current_method:
-        sys.stdout.write("\t".join([ac, iter, run_id, method, topic, accuracy]) )
-        current_method = method
+    match = re.search(regex, line)
 
-    except ValueError:
+    if match:
+      method = match.group(4)
+      if method != current_method:
+        columns = [match.group(i) for i in range(1, 7)]
+        current_method = method
+        print '\t'.join(columns)
+    else:
       continue
